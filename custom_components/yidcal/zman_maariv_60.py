fe93e0a6377d4e3232f878d5fe3c0c7b4c1bfc52
hitchin999 <hitchin999@gmail.com>
2025-07-09 02:32:16 -0400

    Update zman_maariv_60.py

--- custom_components/yidcal/zman_maariv_60.py
+++ custom_components/yidcal/zman_maariv_60.py
@@ -66,10 +66,8 @@ class ZmanMaariv60Sensor(YidCalDevice, RestoreEntity, SensorEntity):
             "maariv_60_with_seconds":   target.isoformat(),
         }
 
-        # ceil to next minute if there's any seconds, else keep the same minute
-        if target.second >= 1:
-            target += timedelta(minutes=1)
-        target = target.replace(second=0, microsecond=0)
+        # ceil to minute if there's any seconds
+        target = (target + timedelta(minutes=1)).replace(second=0, microsecond=0)
 
 
         self._attr_native_value = target.astimezone(timezone.utc)
