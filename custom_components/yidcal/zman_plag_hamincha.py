6096c94353d7205d9b3b6608aa833d96e0071d13
hitchin999 <hitchin999@gmail.com>
2025-10-26 23:58:02 -0400

    Update zman_plag_hamincha.py
    changed device type

--- custom_components/yidcal/zman_plag_hamincha.py
+++ custom_components/yidcal/zman_plag_hamincha.py
@@ -12,11 +12,11 @@ from zmanim.zmanim_calendar import ZmanimCalendar
 from zmanim.util.geo_location import GeoLocation
 
 from .const import DOMAIN
-from .device import YidCalDevice
+from .device import YidCalZmanDevice
 from .zman_sensors import get_geo
 
 
-class PlagHaMinchaSensor(YidCalDevice, RestoreEntity, SensorEntity):
+class PlagHaMinchaSensor(YidCalZmanDevice, RestoreEntity, SensorEntity):
     """פלג המנחה עפ\"י המג\"א (10.75 שעות זמניות)."""
 
     _attr_device_class  = SensorDeviceClass.TIMESTAMP
