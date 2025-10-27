f8b5cf6f0a06eafabb8369ae94a5bc3c27fa6046
hitchin999 <hitchin999@gmail.com>
2025-10-27 00:00:41 -0400

    Update zman_tefilah_mga.py
    changed device type

--- custom_components/yidcal/zman_tefilah_mga.py
+++ custom_components/yidcal/zman_tefilah_mga.py
@@ -12,11 +12,11 @@ from zmanim.zmanim_calendar import ZmanimCalendar
 from zmanim.util.geo_location import GeoLocation
 
 from .const import DOMAIN
-from .device import YidCalDevice
+from .device import YidCalZmanDevice
 from .zman_sensors import get_geo
 
 
-class SofZmanTefilahMGASensor(YidCalDevice, RestoreEntity, SensorEntity):
+class SofZmanTefilahMGASensor(YidCalZmanDevice, RestoreEntity, SensorEntity):
     """סוף-זמן תפילה עפ\"י המג\"א (4 שעות זמניות)."""
 
     _attr_device_class  = SensorDeviceClass.TIMESTAMP
@@ -104,3 +104,4 @@ class SofZmanTefilahMGASensor(YidCalDevice, RestoreEntity, SensorEntity):
             "Tomorrows_Simple": human_tom,
             "Yesterdays_Simple": human_yest,
         }
+
