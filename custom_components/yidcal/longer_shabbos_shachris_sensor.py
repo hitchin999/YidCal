bee8a7b892a6b6870c4aa9d2966b87db41d9123a
hitchin999 <hitchin999@gmail.com>
2026-07-24 01:08:10 -0400

    Update listener registration to use publishing method

--- custom_components/yidcal/longer_shabbos_shachris_sensor.py
+++ custom_components/yidcal/longer_shabbos_shachris_sensor.py
@@ -88,7 +88,7 @@ class LongerShabbosSensor(YidCalSpecialDevice, RestoreEntity, BinarySensorEntity
         self._register_listener(
             async_track_time_change(
                 self.hass,
-                self.async_update,
+                self._publishing(self.async_update),
                 second=0,
             )
         )
